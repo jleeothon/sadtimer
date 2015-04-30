5b221696adc17c60f02a70963743b02626fc0044
jleeothon <jleeothon@gmail.com>
2015-04-29 19:16:50 -0500

    Stopwatch first draft ready

--- sadtimer/stopwatch.js
+++ sadtimer/stopwatch.js
@@ -7,41 +7,66 @@ padNumber = function(n, width, z) {
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-function Timer(milliseconds, element) {
+function Timer(seconds, padding, element) {
   var t = this;
   this.current = 0;
   this.element = element;
   this.id = setInterval(function() {
-    console.log(t.current);
+    t.element.text(padNumber(t.current, padding));
     t.current++;
-  }, milliseconds);
-  this.stop = function() {
-    clearInterval(this.id);
-  };
+  }, seconds * 1000);
+}
+
+TimerFactory = function(template) {
+  this.template = template;
+}
+
+TimerFactory.prototype.secondTimer = function() {
+  var element = this.template.$(".clock .seconds");
+  return new Timer(1, 2, element)
+}
+
+TimerFactory.prototype.minuteTimer = function() {
+  var element = this.template.$(".clock .minutes");
+  return new Timer(60, 2, element);
+}
+
+TimerFactory.prototype.hourTimer = function() {
+  var element = this.template.$(".clock .hours");
+  return new Timer(60 * 60, 3, element);
 }
 
 StopwatchHandler = {
   start: function(template) {
+    var tFactory, hours, minutes, seconds;
     Session.set("stopwatchRunning", true);
-    console.log(template.$(".clock.seconds"));
+    tFactory = new TimerFactory(template);
+    hours = tFactory.hourTimer();
+    minutes = tFactory.minuteTimer();
+    seconds = tFactory.secondTimer();
     Meteor.call("startStopwatch", function(error, result) {
       if (error === undefined) {
         Session.set("stopwatchId", result);
       }
     });
+    Session.set("stopwatchHoursIntervalID", hours.id);
+    Session.set("stopwatchMinutesIntervalID", minutes.id);
+    Session.set("stopwatchSecondsIntervalID", seconds.id);
   },
   lap: function(template) {
     var stopwatchId = Session.get("stopwatchId");
     if (stopwatchId) {
-      console.log(stopwatchId);
       Meteor.call("recordLap", stopwatchId);
-    } else {
-      console.log("no id");
     }
   },
   reset: function(template) {
     Session.set("stopwatchRunning", false);
     Session.set("stopwatchId", null);
+    clearInterval(Session.get("stopwatchHoursIntervalID"));
+    clearInterval(Session.get("stopwatchMinutesIntervalID"));
+    clearInterval(Session.get("stopwatchSecondsIntervalID"));
+    template.$(".clock span").text("00");
+    console.log("hey");
   }
 }
 
@@ -94,6 +119,13 @@ if (Meteor.isClient) {
       return Laps.find({stopwatchId: this._id});
     }
   });
+
+  Template.stopwatchInstance.events({
+    "click button": function(event) {
+      Meteor.call("deleteStopwatch", this._id);
+    }
+  });
+
 }
 
 Meteor.methods({
@@ -101,7 +133,6 @@ Meteor.methods({
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
-    console.log("creating stopwatch");
     return Stopwatches.insert({
       ownerId: Meteor.userId(),
       username: Meteor.user().username,
@@ -115,7 +146,9 @@ Meteor.methods({
       createdAt: new Date()
     });
   },
-  updateStopwatch: function() {
+  deleteStopwatch: function(stopwatchId) {
+    Stopwatches.remove(stopwatchId);
+    Laps.remove({stopwatchId: stopwatchId});
   }
 });
 
